cef0858669a64bb3eec650b00cf5a12b665ef64d
xu42 <xu42.cn@gmail.com>
2016-11-15 20:06:05 +0800

    get IP and MAC

---
+++ README.md
@@ -0,0 +1,59 @@
+# dlpu-network
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]](LICENSE.md)
+[![Total Downloads][ico-downloads]][link-downloads]
+
+dlpu-network 大连工业大学校园网自助服务
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require xu42/dlpu-network
+```
+
+## Usage
+
+``` php
+require_once './vendor/autoload.php';
+$username = '1305040301';
+$password = 'yourpassword';
+$dlpuNetwork = new Xu42\DlpuNetwork\DlpuNetwork($username, $password);
+$config = $dlpuNetwork->getConfig();	// IP 和 MAC 地址等配置信息
+```
+
+## Change log
+
+Please see [CHANGELOG](CHANGELOG.md) for more information what has changed recently.
+
+## Testing
+
+Tests unavailable.
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+## Security
+
+If you discover any security related issues, please using the issue tracker.
+
+## Credits
+
+- [Xu42](https://github.com/xu42)
+- [All Contributors](https://github.com/xu42/dlpu-network/contributors)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/xu42/dlpu-network.svg?style=flat-square
+[ico-license]: https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/xu42/dlpu-network.svg?style=flat-square
+
+[link-packagist]: https://packagist.org/packages/xu42/dlpu-network
+[link-downloads]: https://packagist.org/packages/xu42/dlpu-network
+[link-author]: https://github.com/xu42
+[link-contributors]: ../../contributors
---
+++ src/DlpuNetwork.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Xu42\DlpuNetwork;
+
+
+/**
+ * 大连工业大学校园网自助服务
+ * Class DlpuNetwork
+ * @package Xu42\DlpuNetwork
+ */
+class DlpuNetwork
+{
+    private $username;
+    private $password;
+    private $cookie;
+    private $urlConfig = 'http://210.30.48.36:8080/selfservice/module/webcontent/web/networkdevice_list.jsf';
+
+
+    /**
+     * DlpuNetwork constructor.
+     * @param $username
+     */
+    public function __construct($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->cookie   = $this->getCookie();
+    }
+
+    /**
+     * 获取配置信息
+     * @return array
+     */
+    public function getConfig()
+    {
+        if (!$this->cookie) {
+            return [];
+        }
+
+        $content = $this->myCurl($this->urlConfig);
+
+        preg_match_all('/grayBackground">(.*?)</', $content, $matches);
+
+        return $matches[1];
+    }
+
+
+    /**
+     * 简单封装的get网络请求助手
+     * @return mixed
+     */
+    private function myCurl($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_COOKIE, $this->cookie);
+        $content = curl_exec($ch);
+        if (curl_errno($ch)) {
+            return null;
+        }
+        curl_close($ch);
+        return $content;
+    }
+
+
+    /**
+     * 获取 cookie
+     * @return mixed
+     */
+    public function getCookie()
+    {
+        $cookie = (new ToolLogin($this->username, $this->password))->getCookie();
+
+        return $cookie;
+    }
+
+}
